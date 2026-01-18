ad652a8470f005e31328d119adcf89b6c89686fe
mugbee <mugbee2025@gmail.com>
2026-01-18 18:53:19 +0900

    feat: 🚀  microCMSを利用したブログコレクションを定義する

---
+++ src/content/microcms.ts
@@ -0,0 +1,12 @@
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export const createMicroCMSLoader = (endpoint: string) => {
+  return async () => {
+    return client.getAllContents({ endpoint });
+  };
+};
