b0bcc7fcf8553d60a6eb7847d9448a030469acc7
mugbee <mugbee2025@gmail.com>
2026-01-18 19:53:15 +0900

    feat: 🚀  ソーシャルリンクとフッターを再構成する

--- src/consts/site.ts
+++ src/consts/site.ts
@@ -1,3 +1,23 @@
+// site
 export const SITE_TITLE = "サモンズボードざっくり冒険録";
 export const SITE_DESCRIPTION =
   "スマホゲーム「サモンズボード」の攻略ブログです。";
+
+// footer
+export const SOCIAL_LINKS = [
+  {
+    href: "https://github.com/mugbeeBlog/mugbeeBlog.github.io",
+    title: "GitHub",
+    icon: "github",
+  },
+  {
+    href: "https://www.youtube.com/channel/UC5UfMqhWwMPSSPzuG2SZE-w",
+    title: "YouTube",
+    icon: "youtube",
+  },
+  {
+    href: "https://x.com/mugbee_summons",
+    title: "X（旧Twitter）",
+    icon: "x",
+  },
+] as const;
